33603af163e5603a8b659cdcd62b7d45faab4280
Jaki Sharief Shaik <shaikj@oregonstate.edu>
2017-11-14 11:12:39 -0800

    Update smallsh.c

--- smallsh.c
+++ smallsh.c
@@ -10,12 +10,19 @@ int main(int argc,char * argv[]) {
   size_t inputlength = 0;
   int noofcharsread;
   char * startcwd;
+  char * found;
+  char ** inputarr;
   //memset(startcwd,'\0',400);                                                                                                                                                                              
   while(1) {
     printf(": ");
     noofcharsread = getline(&input,&inputlength,stdin);
+    //printf("user input is %s\n",input);                                                                                                                                                                   
     input[strcspn(input, "\n")]='\0';
-    if(strcmp(input,"status") >= 0) {
+    printf("user input is %s\n",input);
+    while((found = strsep(&input," ")) != NULL)
+      printf("%s\n",found);
+    printf("input after strsep is %s\n",input);
+    if(strcmp(input,"status") == 0) {
       printf("it is in status\n");
     }
     else if (strcmp(input,"cd") == 0) {
@@ -23,12 +30,28 @@ int main(int argc,char * argv[]) {
       printf("it is in cd and cwd is %s\n",startcwd);
       if(argc == 1) {
         printf("arg is %s\n",argv[0]);
-        execlp("cd","cd",NULL);
+        chdir("/nfs/stak/users/shaikj");
+        //pid_t cdpid = fork();                                                                                                                                                                             
+        //printf("cdpid is %d\n",cdpid);                                                                                                                                                                    
+        //if(cdpid == 0) {                                                                                                                                                                                  
+        //printf("it is in child process\n");                                                                                                                                                               
+          //execlp("cd","cd",NULL);                                                                                                                                                                         
+          //chdir("~");                                                                                                                                                                                     
+        //}                                                                                                                                                                                                 
+        //int inttocheckcd;                                                                                                                                                                                 
+        //cdpid = waitpid(cdpid,&inttocheckcd,0);                                                                                                                                                           
       }
       else {
         printf("argc is %d\n",argc);
-        argv[argc] = NULL;
-        execvp(argv[0],argv);
+        //argv[argc] = NULL;                                                                                                                                                                                
+        printf("argv[1] is %s\n",argv[1]);
+        chdir(argv[1]);
+        //pid_t cdspid = fork();                                                                                                                                                                            
+        //if(cdspid == 0) {                                                                                                                                                                                 
+        //execvp(argv[0],argv);                                                                                                                                                                             
+        //}                                                                                                                                                                                                 
+        //int inttocheckcd;                                                                                                                                                                                 
+        //cdspid = waitpid(cdspid,&inttocheckcd,0);                                                                                                                                                         
       }
 
     }
@@ -39,7 +62,7 @@ int main(int argc,char * argv[]) {
       execlp("ls","ls",NULL);
       }
       int inttocheck;
-      lspid = waitpid(lspid,&inttocheck,WNOHANG);
+      lspid = waitpid(lspid,&inttocheck,0);
     }
     else if (strcmp(input,"exit")==0) {
         free(input);
@@ -52,3 +75,4 @@ int main(int argc,char * argv[]) {
   }
 
 }
+
